c5af40d729801ff95308cc85d8822864b87a625f
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-04-11 22:51:04 +0300

    Task_1

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,8 +1,35 @@
 package com.vladarsenjtev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("Яблоко");
+        strings.add("Ананас");
+        strings.add("Банан");
+        strings.add("Яблоко");
+        strings.add("Яблоко");
+        strings.add("Апельсин");
+        strings.add("Персик");
+        strings.add("Арбуз");
+        strings.add("Арбуз");
+        strings.add("Виноград");
+
+        System.out.println(countOcurrance(strings, "Яблоко"));
+
+        }
+
+    public static int countOcurrance(ArrayList<String> list, String str){
+        int count = 0;
+        for (String s : list) {
+            if (s.equals(str)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
