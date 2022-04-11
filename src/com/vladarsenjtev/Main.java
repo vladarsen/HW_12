91a618a19526cb9146d038e4c5f300f9d52687a4
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-04-11 23:05:03 +0300

    Task_2

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,8 +1,23 @@
 package com.vladarsenjtev;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] arr = {1, 2, 3};
+
+        System.out.println(toList(arr));
     }
+
+    public static ArrayList<Integer> toList(int[] arr) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int j : arr) {
+            Collections.addAll(arrayList, j);
+        }
+        return arrayList;
+     }
 }
