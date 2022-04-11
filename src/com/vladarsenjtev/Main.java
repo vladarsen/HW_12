cfc59ccec405b7ed4828de33bedbebfed0b939de
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-04-11 23:19:32 +0300

    Task_3

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -1,8 +1,27 @@
 package com.vladarsenjtev;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Integer> strings = new ArrayList<>();
+        strings.add(7);
+        strings.add(9);
+        strings.add(9);
+        strings.add(9);
+        strings.add(2);
+        strings.add(9);
+        strings.add(5);
+
+        System.out.println(strings);
+        System.out.println(findUnique(strings));
+
+    }
+    public static Set<Integer> findUnique(ArrayList<Integer> arrayList) {
+        Set<Integer> uniqueInteger = new HashSet<Integer>(arrayList);
+        return uniqueInteger;
     }
 }
